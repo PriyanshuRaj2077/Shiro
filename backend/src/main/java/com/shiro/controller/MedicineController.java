ee6665715aa5da4dac0fd1acc318fe00b15de6ee
Priyanshuraj2077 <priyanshuraj2077@gmail.com>
2026-07-21 17:23:28 +0530

    started backend

--- backend/src/main/java/com/shiro/controller/MedicineController.java
+++ backend/src/main/java/com/shiro/controller/MedicineController.java
@@ -1,4 +1,28 @@
 package com.shiro.controller;
 
-public class MedicineController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+//@RestController
+//@RequestMapping(/"api/medicine")
+//public class MedicineController {
+//
+//    @GetMapping("/hello")
+//    public String hello() {
+//        return "Hello from Shiro!";
+//    }
+//}
+
+@RestController
+@RequestMapping("/api/medicine")
+public class MedicineController{
+    @GetMapping("/search")
+    public String search(@RequestParam String name){
+         if (name.equals("paracetamole")){
+            return "details of paracetamole";}
+        }
+        return "Medicine not found";
+    }
 }
