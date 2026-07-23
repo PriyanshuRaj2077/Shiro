6036d610dada326bef9f1a72ca0cb4cc0b265599
Priyanshuraj2077 <priyanshuraj2077@gmail.com>
2026-07-23 12:56:56 +0530

    day 3 of making backend

--- backend/src/main/java/com/shiro/controller/MedicineController.java
+++ backend/src/main/java/com/shiro/controller/MedicineController.java
@@ -1,28 +1,24 @@
 package com.shiro.controller;
 
+import com.shiro.service.MedicineService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-//@RestController
-//@RequestMapping(/"api/medicine")
-//public class MedicineController {
-//
-//    @GetMapping("/hello")
-//    public String hello() {
-//        return "Hello from Shiro!";
-//    }
-//}
 
 @RestController
 @RequestMapping("/api/medicine")
 public class MedicineController{
+
+    private final MedicineService medicineService;
+
+    public MedicineController(MedicineService medicineService){
+        this.medicineService = medicineService;
+    }
+
     @GetMapping("/search")
     public String search(@RequestParam String name){
-         if (name.equals("paracetamole")){
-            return "details of paracetamole";}
-        }
-        return "Medicine not found";
+        return medicineService.searchMedicine(name);
     }
 }
--- backend/src/main/java/com/shiro/service/MedicineService.java
+++ backend/src/main/java/com/shiro/service/MedicineService.java
@@ -1,4 +1,13 @@
 package com.shiro.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class MedicineService {
+    public String searchMedicine(String name){
+        if(name.equals("paracetamol")){
+            return "details of paracetamol";
+        }
+        return "Medicine not found";
+    }
 }
